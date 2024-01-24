a395acac7c7227c4eca3b64ce2ee542ebf228ed3
Sumin13 <su184913@icloud.com>
2024-01-24 18:10:53 +0000

    challenge code 23.01.24

--- event.js
+++ event.js
@@ -1,26 +1,37 @@
-const hi = document.querySelector(".hi");
+// const hi = document.querySelector(".hi");
 
-function mouseOnHi() {
-  hi.style.color = "blue";
-  hi.innerText = "The mouse is here";
-}
+// function mouseOnHi() {
+//   hi.style.color = "blue";
+//   hi.innerText = "The mouse is here";
+// }
 
-function mouseLeft() {
-  hi.style.color = "tomato";
-  hi.innerText = "The mouse just left";
-}
+// function mouseLeft() {
+//   hi.style.color = "tomato";
+//   hi.innerText = "The mouse just left";
+// }
 
-function pageSizing() {
-  document.body.style.backgroundColor = "pink";
-}
+// function pageSizing() {
+//   document.body.style.backgroundColor = "pink";
+// }
 
-function rightClicked() {
-  document.body.innerText = "its been clicked";
-}
-hi.addEventListener("mouseenter", mouseOnHi);
+// function rightClicked() {
+//   document.body.innerText = "its been clicked";
+// }
+// hi.addEventListener("mouseenter", mouseOnHi);
 
-hi.addEventListener("mouseleave", mouseLeft);
+// hi.addEventListener("mouseleave", mouseLeft);
 
-window.addEventListener("resize", pageSizing);
+// window.addEventListener("resize", pageSizing);
 
-window.addEventListener("contextmenu", rightClicked);
+// window.addEventListener("contextmenu", rightClicked);
+
+// let num = 123;
+
+// let str = num.toString();
+
+// console.log(str);
+var num = 24;
+var str = num.toString();
+
+console.log(num); // 24
+console.log(str); // "24"
